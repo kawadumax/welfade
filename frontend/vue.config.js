9fa483e38fca38f50409bca76072722724d2fa6b
kawadumax <kazmax7@gmail.com>
2020-06-01 18:04:06 +0900

    TS, Router, Vuex, Axiosを追加

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    devServer: {
-        // port: 8888,
-        disableHostCheck: true,
-        progress: false
-    },
-};
+  "devServer": {
+    "disableHostCheck": true,
+    "progress": false
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
