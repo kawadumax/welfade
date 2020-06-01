e98253376a6482cecbe8a76352f93b5ad2435c21
kawadumax <kazmax7@gmail.com>
2020-06-01 15:06:36 +0900

    Refactoring

--- backend/src/actions.rs
+++ backend/src/actions.rs
@@ -2,21 +2,6 @@
 use diesel::prelude::*;
 use crate::models;
 
-/// Run query using Diesel to insert a new database row and return the result.
-// pub fn find_user_by_uid(
-//     uid: Uuid,
-//     conn: &SqliteConnection,
-// ) -> Result<Option<models::User>, diesel::result::Error> {
-//     use crate::schema::users::dsl::*;
-
-//     let user = users
-//         .filter(id.eq(uid.to_string()))
-//         .first::<models::User>(conn)
-//         .optional()?;
-
-//     Ok(user)
-// }
-
 pub fn find_user_by_id(
     _id: i32,
     conn: &PgConnection,
