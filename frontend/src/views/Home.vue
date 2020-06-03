73521466b5ea4eb0d7584991908809644cba264b
kawadumax <kazmax7@gmail.com>
2020-06-03 13:22:48 +0900

    LoginとHomeをts化

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <p>{{user}}</p>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from "vue"
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+export type Data = {
+  user: string
 }
+
+export default Vue.extend({
+  name: 'Home',
+  created(): void {
+    console.log("Created!!!")
+    this.$axios.get("http://127.0.0.1:3000/user/2")
+      .then(response => {
+        console.log('status:', response.status) // 200
+        console.log('body:', response.data)     // response body.)
+        this.user = response.data
+      })
+      .catch(err => {
+        console.log('err:', err);
+    })
+  },
+  components: {
+  },
+  data(): Data {
+    return {
+      user: "hoge",
+    }
+  }
+
+})
 </script>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="@/assets/logo.png">
     <p>this is an login page</p>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
 // @ is an alias to /src
-export default {
-  name: 'Home',
+export default Vue.extend({
+  name: 'Login',
   components: {
     // HelloWorld
   }
-}
+})
 </script>
