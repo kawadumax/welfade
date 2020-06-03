c44c04bf6dab13481a99c07de52c107cb6fba82c
kawadumax <kazmax7@gmail.com>
2020-06-03 14:26:37 +0900

    アプリケーションレイアウトを作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,61 @@
 <template>
   <v-app>
+    <v-app-bar app=true>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>就労移行支援情報管理システム Welfade</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer app=true>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <!-- Sizes your content based upon application components -->
     <v-content>
-      <router-view></router-view>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid=true>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   data: () => ({
     //
-  }),
+  })
 });
 </script>
