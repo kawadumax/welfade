1694d61b04b80b3917d50afc811504f0a18508bc
kawadumax <kazmax7@gmail.com>
2020-06-03 17:39:07 +0900

    バリデーション付きフォームを用意

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app=true>
+    <v-app-bar :app="true">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-toolbar-title>就労移行支援情報管理システム Welfade</v-toolbar-title>
@@ -30,20 +30,20 @@
       </v-menu>
     </v-app-bar>
 
-    <v-navigation-drawer app=true>
+    <v-navigation-drawer :app="true">
       <!-- -->
     </v-navigation-drawer>
 
     <!-- Sizes your content based upon application components -->
     <v-content>
       <!-- Provides the application the proper gutter -->
-      <v-container fluid=true>
+      <v-container :fluid="true" fill-height>
         <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
     </v-content>
 
-    <v-footer app>
+    <v-footer :app="true">
       <!-- -->
     </v-footer>
   </v-app>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,17 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <p>this is an login page</p>
-  </div>
+  <v-row align="center" justify="center">
+    <v-form ref="form" v-model="valid">
+      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="submit">Submit</v-btn>
+    </v-form>
+  </v-row>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+
 // @ is an alias to /src
 export default Vue.extend({
-  name: 'Login',
+  name: "Login",
   components: {
     // HelloWorld
+  },
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v: string) => !!v || "Name is required",
+      (v: string) => (v && v.length <= 10) || "Name must be less than 10 characters"
+    ],
+    email: "",
+    emailRules: [
+      (v: string) => !!v || "E-mail is required",
+      (v: string) => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+  }),
+  computed:{
+  // In component, import `VForm`
+    form() :any {
+      return this.$refs.form;
+    }
+  },
+  methods: {
+    submit(){
+      console.log("Submit")
+    },
+    validate() {
+      this.form.validate();
+    },
+    reset() {
+      this.form.reset();
+    },
+    resetValidation() {
+      this.form.resetValidation();
+    }
   }
-})
+});
 </script>
