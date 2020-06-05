f2263a93f3526bed9c76c78d2b21daf2f02c8427
kawadumax <kazmax7@gmail.com>
2020-06-05 16:51:55 +0900

    routing追加、loginページ削除

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
-import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
+import Signin from '@/views/Signin.vue'
 
 Vue.use(VueRouter)
 
@@ -20,9 +21,14 @@ Vue.use(VueRouter)
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
   }
 ]
 
